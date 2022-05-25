5d3a3cd0a438693707330d2c5121905c01f85be3
tks-sugita <98331263+tks-sugita@users.noreply.github.com>
2022-05-25 21:47:16 +0900

    購入機能のリファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
     if @item.user_id == current_user.id
       return  redirect_to root_path
@@ -12,12 +12,7 @@ class OrdersController < ApplicationController
     end
   end
 
-  def new
-    @order_address = OrderAddress.new
-  end
-
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -41,5 +36,9 @@ class OrdersController < ApplicationController
         card: order_params[:token],
         currency: 'jpy'
       )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
   before do
-    @order_address = FactoryBot.build(:order_address)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+    sleep 0.9
   end
 
   describe '配送先情報の保存' do
@@ -58,7 +61,6 @@ RSpec.describe OrderAddress, type: :model do
       it '郵便番号が空だと保存できないこと' do
         @order_address.zip_code = nil
         @order_address.valid?
-
         expect(@order_address.errors.full_messages).to include('Zip code is invalid. Include hyphen(-)')
       end
       it '郵便番号にハイフンがないと保存できないこと' do
@@ -93,6 +95,11 @@ RSpec.describe OrderAddress, type: :model do
       end
       it '電話番号にハイフンがあると保存できないこと' do
         @order_address.tel_number = '123 - 1234 - 1234'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Tel number is invalid')
+      end
+      it '電話番号が9桁以下であると保存できないこと' do
+        @order_address.tel_number = 12_345_678
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Tel number is invalid')
       end
